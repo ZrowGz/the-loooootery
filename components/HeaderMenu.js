6d40bae3d06b02e298d0e5a9509b1f8f9f917ad7
zrowgz <zrowgz@hotmail.com>
2022-02-01 18:18:25 -0800

    removed unnecessary code. verified working

---
+++ components/HeaderMenu.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+import { styleLink, styleConnectButton, styleMenuItem } from './SharedStyles';
+import Link from 'next/link';
+import Typography from '@mui/material/Typography';
+
+export default function HeaderMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls="basic-menu"
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        style={styleConnectButton}
+      >
+          Routes
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose} style={styleMenuItem}>
+            <Link href="/" as="/" style={styleLink}>
+                <Typography> The Train Station </Typography>
+            </Link>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
--- versions/truffle/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var DevilTrainLotto = artifacts.require("./DevilTrainLotto.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(DevilTrainLotto);
-};
